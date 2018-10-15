97ce104892c8244e7eac7f3b643f548ededa583d
Amrutasri <42315519+Amrutasri@users.noreply.github.com>
2018-10-15 11:49:34 +0530

    Add files via upload

---
+++ Twofer.java
@@ -0,0 +1,12 @@
+import java.util.Objects;
+
+class Twofer {
+    String twofer(String name) {
+        String nullString = null;
+        if(Objects.equals(name,nullString)) {
+            return "One for you, one for me.";
+        } else {
+            return "One for " +name+ ", one for me.";
+        }
+    }
+}
